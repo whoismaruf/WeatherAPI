e4b655523a58a30dfd1d97f04be2e3724ccd598f
Maruf Khan <whoismaruf@outlook.com>
2021-09-07 12:18:45 +0600

    Fixed city not found!

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
 console.log("It's working!");
+
+// Get all ID of search area
 const searchButton = document.getElementById('searchButton');
 const spinner = document.getElementById('spinner');
 const spinnerText = document.getElementById('spinner-text');
 const cityInput = document.getElementById('city-input');
 
+// Get all ID of search result section
+const searchResultSection = document.getElementById('search-result');
+const imgIcon = document.getElementById('weather-icon');
+const cityName = document.getElementById('city-name');
+const weatherTemp = document.getElementById('weather-temp');
+const weatherCondition = document.getElementById('weather-condition');
+const cityNotFound = document.getElementById('city-not-found');
+
+
 const API_KEY = '560bf65a383c8af83f6edc08e83b7047';
+
+// Disable the search button for null input
 
 const toggleSpinnerButton = (signal) => {
     if (signal) {
@@ -31,10 +44,33 @@ const showResults = (data) => {
     toggleSpinnerButton(false);
     cityInput.value = '';
     console.log(data);
+    if (data.cod === '404') {
+        cityNotFound.classList.remove('d-none');
+    }
+    else {
+        cityNotFound.classList.add('d-none');
+        imgIcon.setAttribute('src', `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+        cityName.innerText = data.name;
+        weatherTemp.innerText = data.main.temp;
+        weatherCondition.innerText = data.weather[0].main;
+    }
 }
 
 const searchButtonHandler = () => {
-    toggleSpinnerButton(true);
-    const API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${API_KEY}`
-    fetchData(API_ENDPOINT)
+    if (cityInput.value) {
+        toggleSpinnerButton(true);
+        clearOutput();
+        const API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput.value}&appid=${API_KEY}&units=metric`
+        fetchData(API_ENDPOINT)
+    }
+    else {
+        console.log('Input required!');
+    }
+}
+
+const clearOutput = () => {
+    imgIcon.setAttribute('src', "");
+    cityName.innerText = "";
+    weatherTemp.innerText = "";
+    weatherCondition.innerText = "";
 }
